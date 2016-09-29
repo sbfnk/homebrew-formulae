541937e3fafd731e62409cd68c258320b2f41e3e
sbfnk <sbfnk@users.noreply.github.com>
2016-09-29 09:29:59 +0100

    thrust head version

--- thrust.rb
+++ thrust.rb
@@ -4,6 +4,7 @@ class Thrust < Formula
   url "https://github.com/thrust/thrust/releases/download/1.8.2/thrust-1.8.2.zip"
   version "1.8.2"
   sha256 "00925daee4d9505b7f33d0ed42ab0de0f9c68c4ffbe2a41e6d04452cdee77b2d"
+  head "https://github.com/thrust/thrust.git"
 
   def install
     (include/"thrust").install Dir["*"]
