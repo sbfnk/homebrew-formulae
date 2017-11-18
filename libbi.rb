ddee4ab1fd8d1a148eaca847f8b2f9b3331515e7
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2017-11-18 10:20:57 +0000

    try something else

--- libbi.rb
+++ libbi.rb
@@ -149,7 +149,8 @@ class Libbi < Formula
       :PATH => perl_dir.chomp.concat(":\$PATH"),
       :PERL5LIB => ENV["PERL5LIB"].chomp.concat(":$PERL5LIB"),
       :CPPFLAGS => "\$CPPFLAGS -I#{HOMEBREW_PREFIX}/include",
-      :LDFLAGS => "\$LDFLAGS -L$(brew --prefix netcdf)/lib -L$(brew --prefix hdf5)/lib -L$(brew --prefix curl) -lhdf5 -lhdf5_ml -lcurl",
+      # :LDFLAGS => "\$LDFLAGS -L$(brew --prefix netcdf)/lib -L$(brew --prefix hdf5)/lib -L$(brew --prefix curl) -lhdf5 -lhdf5_ml -lcurl",
+      :LD_LIBRARY_PATH => "$(brew --prefix netcdf)/lib:$(brew --prefix hdf5)/lib:$(brew --prefix curl)/lib",
       :CXX => ENV["CXX"],
     }
     bin.env_script_all_files(libexec/"bin", env)
